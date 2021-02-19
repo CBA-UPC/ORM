b2c6a1d09ffd5b82ff1913f724e7c0dab4de776e
Ismael-Castell <icastell@ac.upc.edu>
2021-02-19 18:30:16 +0100

    Fixed deadlock fix error.

--- code/db_manager.py
+++ code/db_manager.py
@@ -244,13 +244,13 @@ class Db(object):
             cursor.execute(request, tuple(values))
         except MySQLdb.Error as error:
             deadlock = 0
-            if re.search('Deadlock', error):
+            if re.search('Deadlock', str(error)):
                 deadlock = 1
             while deadlock:
                 try:
                     cursor.execute(request % tuple(values))
                 except MySQLdb.Error as e:
-                    if not re.search('Deadlock', e):
+                    if not re.search('Deadlock', str(e)):
                         deadlock = 0
                         error = e
                 else:
