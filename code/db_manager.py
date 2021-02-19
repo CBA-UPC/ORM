4e6844b1a1227f3a12f45d9e8314ddb254d755d5
Ismael-Castell <icastell@ac.upc.edu>
2021-02-19 18:23:21 +0100

    Revert "Deadlock fix fix"
    This reverts commit a1eaea67

--- code/db_manager.py
+++ code/db_manager.py
@@ -248,8 +248,6 @@ class Db(object):
                 deadlock = 1
             while deadlock:
                 try:
-                    cursor.close()
-                    cursor = self.conn.cursor(MySQLdb.cursors.DictCursor)
                     cursor.execute(request % tuple(values))
                 except MySQLdb.Error as e:
                     if not re.search('Deadlock', str(e)):
@@ -262,11 +260,12 @@ class Db(object):
             logger.error("SQL ERROR: " + str(error) + "\n-----------------")
             cursor.close()
             return 0
-        self.conn.commit()
-        if log:
-            logger.debug("REQUEST OK.\n-----------------")
-        cursor.close()
-        return -1
+        else:
+            self.conn.commit()
+            if log:
+                logger.debug("REQUEST OK.\n-----------------")
+            cursor.close()
+            return -1
 
     def _delete(self, table, conditions, values, log=None):
         """ Creates a standard DELETE request. """
