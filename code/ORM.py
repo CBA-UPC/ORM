5d96fa9f1a8052ba34680a7b84ae8314f545a58c
ismael-castell <48475606+ismael-castell@users.noreply.github.com>
2025-01-29 09:52:38 +0100

    Small bug fix

--- code/ORM.py
+++ code/ORM.py
@@ -84,7 +84,7 @@ def main(process):
             if re.search("empty", str(e)):
                 work_queue_lock.release()
                 my_dict = driver.capabilities
-                folder = os.path.join(temp_folder, "tmp-" + str(process)
+                folder = os.path.join(temp_folder, "tmp-" + str(process))
                 status_queue_lock.acquire()
                 status_queue.append([str(process), folder, "", os.getpid(), driver.service.process.pid, my_dict['moz:processID'], datetime.now()])
                 status_queue_lock.release()
